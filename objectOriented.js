d4c3c2601908ed5e065091897ca54f585544ebe0
MakWigglz <wigglz734@gmail.com>
2024-10-21 01:09:39 +0300

    Update objectOriented.js

--- objectOriented.js
+++ objectOriented.js
@@ -5,3 +5,20 @@ function Class (string1, string2){
 Class.prototype.display = function(){
   return this.string1 + "and this " + this.string2;
   };
+function Constructor(array){
+  this.array=array;
+}
+String3.prototype.display = function(){
+  var str="";
+}
+for (var i=0; i<this.array.length; i++)
+    str += this.array[i].display() + " ";
+        return str;
+};
+
+
+
+
+
+
+
