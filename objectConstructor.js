f61c0fb2bac6f0fd7d33f0895a8032aeb12d1063
MakWigglz <wigglz734@gmail.com>
2024-10-21 22:36:13 +0300

    Create objectConstructor.js

---
+++ objectConstructor.js
@@ -0,0 +1,13 @@
+function Person(first, last, profession, power) {
+  this.firstName = first;
+  this.lastName = last;
+  this.prof = profession;
+  this.power = power;
+}
+const mySelf = new Person("A", "Mak", "Leisure", "ILoveHer");
+const myLove = new Person("Randa", "K", "L", "allShallLoveMeAndDespair");
+
+
+
+
+
