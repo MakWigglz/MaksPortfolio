f2bcbc54d6c04fdd524611a0fae0dea000510290
MakWigglz <wigglz734@gmail.com>
2025-04-19 00:19:55 +0300

    Create circle_class.py

---
+++ py-functions-practice/circle_class.py
@@ -0,0 +1,11 @@
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
+    
+circle1 = Circle(10)
+circle2 = Circle(7)
+
