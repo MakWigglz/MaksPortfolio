c539e48339283f3d725d268943646bc3c8c2f025
MakWigglz <wigglz734@gmail.com>
2025-05-10 02:18:34 +0300

    Update maks_combined.py

--- scipy-numpy/maks_combined.py
+++ scipy-numpy/maks_combined.py
@@ -19,3 +19,27 @@ print("2D Array:\n", array_2d)
 print("1D Array for Addition:\n", array_add)
 print("1D Array for Multiplication:\n", array_mult)
 print("Result of Combined Broadcasting and Vectorization:\n", final_result)
+
+"""
+    (new-matplot-venv) amakki@As-MacBook-Pro numpy % python3 maks_vectorization.py
+Array A:
+ [1 2 3 4 5]
+Array B:
+ [10 20 30 40 50]
+Result of Vectorized Multiplication:
+ [ 10  40  90 160 250]
+(new-matplot-venv) amakki@As-MacBook-Pro numpy % python3 maks_combined.py
+2D Array:
+ [[1 2 3]
+ [4 5 6]
+ [7 8 9]]
+1D Array for Addition:
+ [10 20 30]
+1D Array for Multiplication:
+ [1 2 3]
+Result of Combined Broadcasting and Vectorization:
+ [[ 11  22  33]
+ [ 28  50  72]
+ [ 51  84 117]]
+(new-matplot-venv) amakki@As-MacBook-Pro numpy % 
+"""
